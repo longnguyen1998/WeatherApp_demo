bf59302f68097019ace42efa863f1dc9302dc714
longnguyen1998 <thanhlong121998@gmail.com>
2019-11-25 17:19:10 +0700

    Initial Commit

---
+++ Weather_HaNoi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather_HaNoi
+//
+//  Created by Nguyễn Đình Thành Long on 11/25/19.
+//  Copyright © 2019 Nguyễn Đình Thành Long. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
